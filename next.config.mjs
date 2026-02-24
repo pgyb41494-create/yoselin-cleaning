1bcad410f9848837e9a45eac6f42b39e764ee0d7
Jx4iivx <pgyb41494@gmail.com>
2026-02-24 16:36:52 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+        ],
+      },
+    ];
+  },
+};
 export default nextConfig;
